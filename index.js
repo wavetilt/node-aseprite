99c778b7948184e19ab7886c469c035d20220d50
Josh Junon <junon@wavetilt.com>
2020-07-27 10:03:53 +0200

    add clean function

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const Aseprite = require('./aseprite');
 const KaitaiStream = require('kaitai-struct/KaitaiStream');
 
+const cleanProps = [
+	'_io',
+	'_parent',
+	'_root',
+	'_dataView',
+	'_byteLength'
+];
+
 function parse(content) {
 	const ase = new Aseprite(new KaitaiStream(content));
 	return ase;
 }
 
+function clean(obj) {
+	if (typeof obj === 'object' && obj !== null) {
+		if (Array.isArray(obj)) {
+			for (const v of obj) {
+				clean(v);
+			}
+		} else {
+			for (const prop of cleanProps) {
+				delete obj[prop];
+			}
+
+			for (const key of Object.keys(obj)) {
+				clean(obj[key]);
+			}
+		}
+	}
+
+	return obj;
+}
+
 exports.parse = parse;
+exports.clean = clean;
 
 exports.default = exports;
